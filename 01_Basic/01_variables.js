331032e585fbfa9de3f72ea217ee70378e10eef9
Suraj-harry <suraj18316@gmail.com>
2025-03-25 14:47:52 +0000

    dataTypes

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "suraj@gmail.com"
+var accountPassword = "Suraj"
+accountCity = "Jaipur"
+
+accountEmail = "hc@gmail.com"
+accountPassword = "2112121"
+accountCity = "Bengaluru"
+let accountState;
+
+// accountId = 2  not allowed in js
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
