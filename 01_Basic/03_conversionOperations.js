f5811fe1a77530a414a2b25b6a465649e79b1275
Suraj-harry <suraj18316@gmail.com>
2025-03-25 15:08:23 +0000

    conversion

---
+++ 01_Basic/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = true
+// let scores = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
