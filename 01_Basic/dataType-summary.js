e658fd67f476582593574164cc21d7fecc54f9d7
Suraj-harry <suraj18316@gmail.com>
2025-03-26 14:45:44 +0000

    DataTypes

---
+++ 01_Basic/dataType-summary.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// 7 Types : String , Number , boolean , null , symbol , BigInt= scientific number or big values cover in BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+// Reference (Non-Primitive)
+
+// Arrays , function , Objects
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
