00d8bad439bfc7787de1fc2f92c34975e56aedc5
Suraj-harry <suraj18316@gmail.com>
2025-04-03 11:40:05 +0000

    switch

---
+++ 04_controlflow/02_switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "May"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Febuary");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+
+    default:
+        console.log("Default Case");
+        break;
+}
